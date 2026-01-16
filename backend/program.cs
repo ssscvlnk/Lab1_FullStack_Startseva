6ef73c603244a16b0c62a653302eaad7c4846aaf
ssscvlnk <pstartceva@yandex.ru>
2026-01-16 11:20:59 +0300

    Added basic backend program

---
+++ backend/program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Меня зовут Старцева П.С.");
+Console.WriteLine(DateTime.Now);
